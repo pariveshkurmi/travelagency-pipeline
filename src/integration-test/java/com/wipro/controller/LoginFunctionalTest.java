e160d6621d4d7e0cc9b2d193882f872eb55f3494
Parivesh <pariveshkurmi.mit@gmail.com>
2018-05-19 15:34:30 +0530

    cross browser plugin

--- src/integration-test/java/com/wipro/controller/LoginFunctionalTest.java
+++ src/integration-test/java/com/wipro/controller/LoginFunctionalTest.java
@@ -19,7 +19,7 @@ public class LoginFunctionalTest {
 	@BeforeClass
 	public static void setup() {
 		//System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver_win32\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/driver/travelagency-pipeline/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "/var/jenkins_home/driver/travelagency-pipeline/chromedriver.exe");
 		
 		driver = new ChromeDriver();
 
